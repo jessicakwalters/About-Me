a0ccafa6b1f2feb5c4f7fc233699738847ee68f6
Jessica Walters <jessicakwalters@gmail.com>
2017-05-25 19:27:11 -0700

    print answers to page

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var yesOrNo = confirm('Hello, ' + user + '. I\'m going to ask you a series of qu
 console.log(yesOrNo);
 
     //Question 1
-    var answer1 = prompt(user + ' does Jessica speak French?');
+    var question1 = 'Does Jessica speak French?';
+    var answer1 = prompt(user + '\: ' + question1);
 
     if (answer1.toUpperCase() === 'Y') {
       alert('Bien fait! She does speak French!');
@@ -13,10 +14,11 @@ console.log(yesOrNo);
       alert('Sorry, you\'re wrong. She really does speak French!.');
     };
 
-    console.log(user + ' input ' + answer1)
+    console.log(question1 + user + ' input ' + answer1)
 
     //Question 2
-    var answer2 = prompt(user + ' does Jessica have a dog?');
+    var question2 = 'Does Jessica have a dog?';
+    var answer2 = prompt(user + '\: ' + question2);
 
     if (answer2.toUpperCase() === 'Y') {
       alert('Yep!  Her dog is named Frank!');
@@ -24,10 +26,11 @@ console.log(yesOrNo);
       alert('Sorry, you\'re wrong. She does have a dog!');
     };
 
-    console.log(user + ' input ' + answer2);
+    console.log(question2 + user + ' input ' + answer2);
 
     //Question 3
-    var answer3 = prompt(user + ' has Jessica been to Fiji?');
+    var question3 = 'Has Jessica been to Fiji?'
+    var answer3 = prompt(user + '\: ' + question3);
 
     if (answer3.toUpperCase() === 'Y') {
       alert('Nope.  She has never been to Fiji');
@@ -35,10 +38,11 @@ console.log(yesOrNo);
       alert('You\'re correct! She has never been.');
     };
 
-    console.log(user + ' input ' + answer3);
+    console.log(question3 + user + ' input ' + answer3);
 
     //Question 4
-    var answer4 = prompt(user + ' does Jessica like to ski?');
+    var question4 = 'Does Jessica like to ski?';
+    var answer4 = prompt(user + '\: ' + question4);
 
     if (answer4.toUpperCase() === 'Y') {
       alert('Yes, she LOVES to ski!');
@@ -46,10 +50,11 @@ console.log(yesOrNo);
       alert('Are you crazy? Of course she likes to ski!');
     };
 
-    console.log(user + ' input ' + answer4);
+    console.log(question4 + user + ' input ' + answer4);
 
     //Question 5
-    var answer5 = prompt(user + ' does Jessica like hot dogs?');
+    var question5 = 'Does Jessica like hot dogs?';
+    var answer5 = prompt(user + '\: ' + question5);
 
     if (answer5.toUpperCase() === 'Y') {
       alert('Wrong.  She does not like hot dogs.');
@@ -57,4 +62,7 @@ console.log(yesOrNo);
       alert('Exactly.  She prefers cheeseburgers.');
     };
 
-    console.log(user + ' input ' + answer5);
+    console.log(question5 + user + ' input ' + answer5);
+
+    //document.write(question1, answer1);
+    document.getElementById('quiz-results').innerHTML = question1 + '<br>' + answer1 + '<br>' + question2 + '<br>' + answer2 + '<br>' + question3 + '<br>' + answer3 + '<br>' + question4 + '<br>' + answer4 + '<br>' + question5 + '<br>' + answer5;
